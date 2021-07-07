78b12effe77639b3cb4d7fa5e127e6644bb5e6e6
stevenvanophem <stevenvanophem@gmail.com>
2021-07-08 01:07:29 +0200

    Remove references to baseentity

--- src/main/java/org/springframework/samples/petclinic/adapter/out/EntityUtils.java
+++ src/main/java/org/springframework/samples/petclinic/adapter/out/EntityUtils.java
@@ -23,39 +23,11 @@ import org.springframework.samples.petclinic.domain.pettype.PetType;
 import org.springframework.samples.petclinic.domain.speciality.Specialty;
 import org.springframework.samples.petclinic.domain.vet.Vet;
 import org.springframework.samples.petclinic.domain.visit.Visit;
-import org.springframework.samples.petclinic.model.BaseEntity;
 
 import java.util.Collection;
 
-/**
- * Utility methods for handling entities. Separate from the BaseEntity class mainly because of dependency on the
- * ORM-associated ObjectRetrievalFailureException.
- *
- * @author Juergen Hoeller
- * @author Sam Brannen
- * @see org.springframework.samples.petclinic.model.BaseEntity
- * @since 29.10.2003
- */
-public abstract class EntityUtils {
 
-    /**
-     * Look up the entity of the given class with the given id in the given collection.
-     *
-     * @param entities    the collection to search
-     * @param entityClass the entity class to look up
-     * @param entityId    the entity id to look up
-     * @return the found entity
-     * @throws ObjectRetrievalFailureException if the entity was not found
-     */
-    public static <T extends BaseEntity> T getById(Collection<T> entities, Class<T> entityClass, int entityId)
-        throws ObjectRetrievalFailureException {
-        for (T entity : entities) {
-            if (entity.getId() == entityId && entityClass.isInstance(entity)) {
-                return entity;
-            }
-        }
-        throw new ObjectRetrievalFailureException(entityClass, entityId);
-    }
+public abstract class EntityUtils {
 
     public static Owner getOwnerById(Collection<Owner> entities, Class<Owner> entityClass, int entityId)
         throws ObjectRetrievalFailureException {
